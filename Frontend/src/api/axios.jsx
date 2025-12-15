4638ebf05f2b5c442318a2722406c7cf6a38b117
shuvam_776 <shuvamsatapathi@gmail.com>
2025-12-15 12:04:51 +0530

    2

--- Frontend/src/api/axios.jsx
+++ Frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://task-manager-ref4-1cmjn2alf-shuvam776s-projects.vercel.app/', 
+  baseURL: 'https://task-manager-ref4-1cmjn2alf-shuvam776s-projects.vercel.app', 
   withCredentials: true,
 });
 
